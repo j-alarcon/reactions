1d500206baa1f4bd85dd4e69e940db5ff944c022
Javier <yard.campbs@gmail.com>
2022-05-04 18:23:22 +0100

    Complete player and computer behaviour

--- js/main.js
+++ js/main.js
@@ -46,38 +46,63 @@ let reactions = [
   ),
 ];
 
-function getNameReaction(element1, element2, reactions) {
+function getReaction(element1, element2, reactions) {
   for (let i = 0; i < reactions.length; i++) {
     for (let j = 0; j < 2; j++) {
       if (
         reactions[i].origin.includes(element1) &&
         reactions[i].origin.includes(element2)
       ) {
-        return reactions[i].getName;
+        return reactions[i];
       }
     }
   }
 }
 
+selections[0].addEventListener("change", () => {
+  document.getElementById("castReaction").removeAttribute("disabled");
+  selections[1].removeAttribute("disabled");
+  selections[1].innerHTML = "";
+  elements.forEach((e) => {
+    if (selections[0].options[selections[0].selectedIndex].value != e) {
+      let option = document.createElement("option");
+      option.innerText = e;
+      option.value = e;
+      document.getElementById(selections[1].appendChild(option));
+    }
+  });
+});
+
 selections.forEach((e) =>
   e.addEventListener("change", () => {
-    document.getElementById("resultReaction").innerText = getNameReaction(
+    document.getElementById("resultReaction").innerText = getReaction(
       selections[0].value,
       selections[1].value,
       reactions
-    );
+    ).getName;
   })
 );
 
-selections[0].addEventListener("change", () => {
-    selections[1].removeAttribute("disabled");
-    selections[1].innerHTML = "";
-    elements.forEach((e) => {
-        if(selections[0].options[selections[0].selectedIndex].value != e){
-            let option = document.createElement("option");
-            option.innerText = e;
-            option.value = e;
-            document.getElementById(selections[1].appendChild(option));
-        }
-    })
-})
+document.getElementById("castReaction").addEventListener("click", () => {
+  playRound(
+    getReaction(selections[0].value, selections[1].value, reactions),
+    getReaction(...computerPlay(elements), reactions)
+  );
+});
+
+function playRound(yourReaction, computerReaction) {
+  document.getElementById("result").innerText =
+    "Your reaction: " +
+    yourReaction.getName +
+    "\nComputer Reaction: " +
+    computerReaction.getName;
+}
+
+function computerPlay(elements) {
+  let firstValue, secondValue;
+  do {
+    firstValue = elements[Math.floor(Math.random() * 4)];
+    secondValue = elements[Math.floor(Math.random() * 4)];
+  } while (firstValue === secondValue);
+  return [firstValue, secondValue];
+}
