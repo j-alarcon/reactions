7a093107d2f6d12ae621d1e014c968ad6ac5f17e
Javier <yard.campbs@gmail.com>
2022-05-04 17:18:20 +0100

    Create prototype of select reaction

--- js/main.js
+++ js/main.js
@@ -1,23 +1,68 @@
 import { Reaction } from "./model.js";
-var selections = [
+
+let selections = [
   document.getElementById("firstReaction"),
   document.getElementById("secondReaction"),
 ];
 
-var water = new Reaction(
-  "Water",
-  ["Fire", "Ice"],
-  ["Snowbot", "heatWave"],
-  ["Explosion", "Blizzard", "heatWave"]
-);
+let reactions = [
+  new Reaction(
+    "Water",
+    ["Fire", "Ice"],
+    ["Snowbot", "heatWave"],
+    ["Explosion", "Blizzard", "heatWave"]
+  ),
+  new Reaction(
+    "HeatWave",
+    ["Fire", "Wind"],
+    ["Snowbot", "Water"],
+    ["Blizzard", "Water"]
+  ),
+  new Reaction(
+    "Explosion",
+    ["Fire", "Machine"],
+    ["Water", "Blizzard"],
+    ["Hurricane", "Snowbot"]
+  ),
+  new Reaction(
+    "Blizzard",
+    ["Ice", "Wind"],
+    ["water", "heatWave"],
+    ["Snowbot", "Explosion"]
+  ),
+  new Reaction(
+    "Snowbot",
+    ["Ice", "Machine"],
+    ["Blizzard", "Explosion"],
+    ["Water", "heatWave"]
+  ),
+  new Reaction(
+    "Hurricane",
+    ["Machine", "Wind"],
+    ["Explosion", "Hurricane"],
+    ["Hurricane"]
+  ),
+];
 
-var blizzard = new Reaction(
-  "Blizzard",
-  ["Ice", "Wind"],
-  [water, "heatWave"],
-  ["Snowbot", "Explosion"]
-);
+function getNameReaction(element1, element2, reactions) {
+  for (let i = 0; i < reactions.length; i++) {
+    for (let j = 0; j < 2; j++) {
+      if (
+        reactions[i].origin.includes(element1) &&
+        reactions[i].origin.includes(element2)
+      ) {
+        return reactions[i].getName;
+      }
+    }
+  }
+}
 
-selections.forEach(e => e.addEventListener("change", () => {
-    document.getElementById("resultReaction").innerText = e.value;
-}))
+selections.forEach((e) =>
+  e.addEventListener("change", () => {
+    document.getElementById("resultReaction").innerText = getNameReaction(
+      selections[0].value,
+      selections[1].value,
+      reactions
+    );
+  })
+);
