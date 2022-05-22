e16f3c569cc220e3e631a1dda09edff91b65a541
Javier <yard.campbs@gmail.com>
2022-05-22 20:57:59 +0100

    Delete console.logs

--- sw.js
+++ sw.js
@@ -58,11 +58,9 @@ const contentToCache = addImagesToArray(appShellFiles, 50);
 
 // Installation of service-workers
 self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
   e.waitUntil(
     (async () => {
       const cache = await caches.open(cacheName);
-      console.log("[Service Worker] Caching all: app shell and content");
       await cache.addAll(contentToCache);
     })()
   );
@@ -73,11 +71,9 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
       const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (r) return r;
       const response = await fetch(e.request);
       const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
       return response;
     })()
