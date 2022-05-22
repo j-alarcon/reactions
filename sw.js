46075b714a2ab2bab2ff44d968d949e03a1299a7
Javier <yard.campbs@gmail.com>
2022-05-22 20:54:17 +0100

    Add console.logs

--- sw.js
+++ sw.js
@@ -45,12 +45,12 @@ const appShellFiles = [
   "./img/portraits/default.jpg",
 ];
 
-function addImagesToArray(array, totalNumber) {
+function addImagesToArray(array, totalNumber){
   const portraits = [];
   for (let i = 1; i <= totalNumber; i++) {
     portraits.push("./img/portraits/" + i + ".jpg");
   }
-  return array.concat(portraits);
+  return array.concat(portraits)
 }
 
 // Merge of images to current cache storage
@@ -58,9 +58,11 @@ const contentToCache = addImagesToArray(appShellFiles, 50);
 
 // Installation of service-workers
 self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
   e.waitUntil(
     (async () => {
       const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
       await cache.addAll(contentToCache);
     })()
   );
@@ -71,9 +73,11 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
       const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (r) return r;
       const response = await fetch(e.request);
       const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
       return response;
     })()
